6874f5238754b951ccdc14cc262a086b79404e28
gtang0131 <garytang@quadanalytix.com>
2015-09-11 02:23:00 -0700

    Finished Lesson1: Added text to listview, Adapter setup

--- app/src/main/java/com/example/garytang/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/garytang/sunshine/MainActivityFragment.java
@@ -5,6 +5,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +23,28 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        //ArrayList<String> foreCastEntry = new ArrayList<String>();
+        //ArrayList populated with dummy forecast texts
+        List<String> foreCastEntry = new ArrayList<String>(Arrays.asList("shit", "son", "this",
+                "data","fake", "as", "hell", "jesus", "on", "loller", "skates"));
+
+        ArrayAdapter<String> foreCastAdapter =
+            new ArrayAdapter<String>(
+                    //context - contains global info about app env, allows us to access system resources
+                    getActivity(),
+                    //ID of list item layout
+                    R.layout.list_item_forecast,
+                    //ID of text view to populate
+                    R.id.list_item_forecast_textview,
+                    //list of dummy data
+                    foreCastEntry);
+
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(foreCastAdapter);
+
+
+        return rootView;
     }
 }
